3c56b7f4af465dfe514db7ab280dea8ea0ab346d
mettagame <70934473+mettagame@users.noreply.github.com>
2022-09-10 22:43:11 +0100

    timer updated

--- data/timer/count.js
+++ data/timer/count.js
@@ -1,32 +1,43 @@
-var startingMinutes = 10;
-let time = startingMinutes * 60;
+/*var startingMinutes = 10;*/
+var startingSeconds = 600;
+let time = startingSeconds; /* startingMinutes * 60; */
 var paused = true;
 var countActive = false;
 
 const countdownElement = document.getElementById('countdown');
 const startButton = document.getElementById("start-button");
-const upButton = document.getElementById("up-button");
+
+const dblDownButton = document.getElementById("down-down-button");
 const downButton = document.getElementById("down-button");
+const upButton = document.getElementById("up-button");
+const dblUpButton = document.getElementById("up-up-button");
+
 const resetButton = document.getElementById("reset-button");
 
-startButton.onclick = function(){StartPause()};
-upButton.onclick = function(){AddMinute()};
-downButton.onclick = function(){RemoveMinute()}
-resetButton.onclick = function(){ResetButton()};
+startButton.addEventListener('click', StartPause);
 
-setInterval(UpdateCountdown, 1000); 
-// run UpdateCountdown every 1000ms
+downButton.addEventListener('click', MinusTen);
+dblDownButton.addEventListener('click', MinusSixty);
+
+upButton.addEventListener('click', AddTen);
+dblUpButton.addEventListener('click', AddSixty);
+
+resetButton.addEventListener('click', ResetButton);
+
+setInterval(UpdateCountdown, 1000);  // run UpdateCountdown every 1000ms
+
+document.addEventListener("DOMContentLoaded", Initialize); //run init on first load
 
 function Initialize()
 {
-    time = startingMinutes * 60;
+    time = startingSeconds; /* startingMinutes * 60; */
     paused = true;
     countActive = false;
     countdownElement.style.color = "white";
     DisplayTime();
 }
 
-function UpdateCountdown()
+function UpdateCountdown() //updates at 1000ms
 {
     if(!paused)
     {
@@ -38,18 +49,55 @@ function Countdown()
 {
     DisplayTime();
     time--;
-    if (time <= 0)
+    
+    if (time <= 30)
     {
-        TimeUp();
-    }
+        if (time <=0)
+        {
+            TimeUp();
+        }
+        else {TimeLow()};
+    }   
+    
 }
 
 function DisplayTime()
 {
-    const mins = Math.floor(time / 60);
+    let mins = Math.floor(time / 60);
+    let hours = Math.floor(time / 60 / 60);
+    if (hours > 0)
+    {
+        mins -= hours * 60;
+    }
+
     let secs = time % 60;
-    secs = secs < 10 ? "0" + secs : secs;
-    countdownElement.innerHTML = `${mins}:${secs}`;
+    
+    if (hours > 0 && mins < 10) //add 0 for single digit mins if hours counted
+    {
+        mins = "0" + mins;
+    }
+
+    secs = secs < 10 ? "0" + secs : secs; //same for seconds under 10 at all times
+    
+    
+    if (hours > 0) //only display hours if relevant
+    {
+        countdownElement.innerHTML = `${hours}:${mins}:${secs}`;
+        countdownElement.className = "hours";
+    }
+    else if (mins > 0)
+    {
+        countdownElement.innerHTML = `${mins}:${secs}`;
+        countdownElement.className = "minutes";
+    }
+    else
+    {
+        countdownElement.innerHTML = `${secs}`;
+        countdownElement.className = "seconds";
+   
+    }
+    
+    
 }
 
 function TimeUp()
@@ -57,6 +105,11 @@ function TimeUp()
     time = 0;
     running = false;
     countdownElement.style.color = "red";
+}
+
+function TimeLow()
+{
+    countdownElement.style.color = "yellow";
 }
 
 function StartPause()
@@ -70,7 +123,7 @@ function StartPause()
     }
     else
     {
-        startButton.className = "blue";
+        startButton.className = "red";
         startButton.innerHTML = "stop";
     }
 }
@@ -83,51 +136,39 @@ function ResetButton()
     startButton.innerHTML = "start";
 }
 
-function AddMinute()
-{
-    if(paused && !countActive)
-    {
-        if (startingMinutes <120)
-        {
-            startingMinutes++;
-            Initialize();
-        }
-    }
-}
 
 function AddTen()
 {
-    if(paused && !countActive)
-    {
-        if (startingMinutes <120)
-        {
-            startingMinutes+=10;
-            if (startingMinutes > 120) startingMinutes = 120;
-            Initialize();
-        }
-    }
+    Increment(30);
 }
 
-function RemoveMinute()
+function AddSixty()
+{
+    Increment(600);
+}
+
+function MinusTen()
+{
+    Increment(-30);
+}
+
+function MinusSixty()
+{
+    Increment(-600);
+}
+
+function Increment(value)
 {
     if(paused && !countActive)
     {
-        if (startingMinutes > 1){
-            startingMinutes--;  
-            Initialize();     
-        }
+        startingSeconds+=value;
+        ValidateTime();
+        Initialize();
     }
 }
 
-function RemoveTen()
+function ValidateTime()
 {
-    if(paused && !countActive)
-    {
-        if (startingMinutes > 10){
-            startingMinutes-=10;
-            if (startingMinutes < 1) startingMinutes = 1; 
-            Initialize();     
-        }
-    }
+    startingSeconds = startingSeconds <=0 ? 0 : startingSeconds;
+    startingSeconds = startingSeconds >= 120*60 ? 120*60 : startingSeconds;
 }
-
